01ca797455e9561fc8afe58a9270c2e29998f4b5
lynchbilly <87466682+lynchbilly@users.noreply.github.com>
2022-12-20 11:54:32 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function hello() {
+  alert("Hello how are you doing?"function hello() {
+  alert("Hello how are you doing?");
+}
+
+function showtime() {
+  var now = new Date();
+  alert(w);
+}
+);
+}
+
+function showtime() {
+  var now = new Date();
+  aler(w);
+}
