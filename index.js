450bbf8466b917a450f35e53f228f8ca82cb6861
Billy Lynch <87466682+lynchbilly@users.noreply.github.com>
2022-12-26 21:21:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function hello() {
-  alert("Hello how are you doing?"function hello() {
-  alert("Hello how are you doing?");
-}
-
-function showtime() {
-  var now = new Date();
-  alert(w);
-}
-);
-}
-
-function showtime() {
-  var now = new Date();
-  aler(w);
-}
+<script type="text/javascript" src="https://form.jotform.com/jsform/223598542069163"></script>
